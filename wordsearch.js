45aea29d7441b48065ef20bba42a9c11194c29b8
Kazuko Baynton <kazukobaynton@gmail.com>
2022-02-24 18:28:10 -0800

    add more tests and add more functions to go through words horizontally, vertically, and in reverse

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
+// creating a function which finds the word in the given arrays
+
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
+    // checks the array horizontally 
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        let reversedL = l.split("").reverse().join("")
+        if (l.includes(word) || reversedL.includes(word)) {
+            return true;
+        }
     }
+
+    //checks the array vertically 
+    for (let i = 0; i < letters.length; i++) {
+        let verticalJoin = letters.map(ls => ls[i]).join('')
+        console.log(verticalJoin);
+        let reversedV = verticalJoin.split("").reverse().join("");
+        if (verticalJoin.includes(word) || reversedV.includes(word)) {
+            return true;
+        }
+    }
+   
+    return false;
+
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
